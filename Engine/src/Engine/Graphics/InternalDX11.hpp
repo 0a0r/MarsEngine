1815597d522ce07b8cd59e01552ef7b9babcdf27
hymscott <hymscott@gmail.com>
2022-05-24 13:42:14 -0500

    Add DX11 and DX12 internal (placeholder now)

---
+++ Engine/src/Engine/Graphics/InternalDX11.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <d3d11.h>
+#include <dxgi.h>
+
+#undef OPAQUE
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
+
+#include <d3dcompiler.h>
+#pragma comment(lib, "d3dcompiler.lib")
+
+#include <dxgidebug.h>
+#pragma comment(lib, "dxguid.lib")
