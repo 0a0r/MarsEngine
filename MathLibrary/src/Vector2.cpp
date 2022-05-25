d673d90c099377f4c93688620fb7a83616c6ee2a
hymscott <hymscott@gmail.com>
2022-05-25 13:22:11 -0500

    Update Vector2

--- MathLibrary/src/Vector2.cpp
+++ MathLibrary/src/Vector2.cpp
@@ -1,1 +1,32 @@
 #include "mathpch.h"
+
+namespace Math
+{
+	Vector2::Vector2(Vector2 const& copyFrom)
+		: x(copyFrom.x), y(copyFrom.y) {}
+
+	float Math::Vector2::Length() const
+	{
+		return sqrt(x * x + y * y);
+	}
+
+	float Math::Vector2::LengthSquared() const
+	{
+		return x * x + y * y;
+	}
+
+	void Math::Vector2::Normalized()
+	{
+		// #ToDo: Fix the bug if length == 0
+		float len = Length();
+		x /= len;
+		y /= len;
+	}
+
+	Vector2 Math::Vector2::GetNormalized() const
+	{
+		Vector2 copy = *this;
+		copy.Normalized();
+		return copy;
+	}
+}
--- MathLibrary/src/Vector2.hpp
+++ MathLibrary/src/Vector2.hpp
@@ -4,6 +4,15 @@ namespace Math
 {
 	struct Vector2
 	{
-		float x, y;
+		float x = 0.f, y = 0.f;
+
+		Vector2() {}
+		Vector2(Vector2 const& copyFrom);
+		~Vector2() {}
+
+		float	Length() const;
+		float	LengthSquared() const;
+		void	Normalized();
+		Vector2	GetNormalized() const;
 	};
 }
