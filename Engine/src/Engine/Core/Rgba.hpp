125e2ab5fa6e0c4d9db49402edd04086184f0d6c
hymscott <hymscott@gmail.com>
2022-05-25 11:12:53 -0500

    Add Rgba8 and Rgba16 (HDR purpose)

---
+++ Engine/src/Engine/Core/Rgba.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+struct Rgba8
+{
+	unsigned char r = 255;
+	unsigned char g = 255;
+	unsigned char b = 255;
+	unsigned char a = 255;
+
+	Rgba8() {};
+	Rgba8(unsigned char red, unsigned char green, unsigned char blue, unsigned char alpha = 255)
+		: r(red), g(green), b(blue), a(alpha) {}
+	
+	bool operator == (Rgba8 const& compare) const 
+	{
+		return r == compare.r && g == compare.g && b == compare.b && a == compare.a;
+	}
+
+	bool operator != (Rgba8 const& compare) const
+	{
+		return r != compare.r || g != compare.g || b != compare.b || a != compare.a;
+	}
+};
+
+struct Rgba16
+{
+	unsigned short r = 65535;
+	unsigned short g = 65535;
+	unsigned short b = 65535;
+	unsigned short a = 65535;
+
+	Rgba16() {};
+	Rgba16(unsigned short red, unsigned short green, unsigned short blue, unsigned short alpha = 65535)
+		: r(red), g(green), b(blue), a(alpha) {}
+
+	bool operator == (Rgba16 const& compare) const
+	{
+		return r == compare.r && g == compare.g && b == compare.b && a == compare.a;
+	}
+
+	bool operator != (Rgba16 const& compare) const
+	{
+		return r != compare.r || g != compare.g || b != compare.b || a != compare.a;
+	}
+};
