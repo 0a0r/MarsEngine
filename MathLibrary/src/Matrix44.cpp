c34d7718a2e6a54fa9a5ca806aa1ccdbbffc9885
hymscott <hymscott@gmail.com>
2022-06-08 09:23:50 -0500

    Update constructor of Matrix44

--- MathLibrary/src/Matrix44.cpp
+++ MathLibrary/src/Matrix44.cpp
@@ -4,6 +4,36 @@ namespace Math
 {
 	Matrix44::Matrix44()
 	{
+		// Identity default
+		mValues[Ix] = 1.f;
+		mValues[Jy] = 1.f;
+		mValues[Kz] = 1.f;
+		mValues[Tw] = 1.f;
+	}
 
+	Matrix44::Matrix44(Vector2 const& iBasis2D, Vector2 const& jBasis2D, Vector2 const& translation2D)
+		: Matrix44()
+	{
+		mRowValues[0].xy = iBasis2D;
+		mRowValues[1].xy = jBasis2D;
+		mRowValues[3].xy = translation2D;
+	}
+
+	Matrix44::Matrix44(Vector3 const& iBasis3D, Vector3 const& jBasis3D, Vector3 const& kBasis3D, Vector3 const& translation3D)
+		: Matrix44()
+	{
+		mRowValues[0].xyz = iBasis3D;
+		mRowValues[1].xyz = jBasis3D;
+		mRowValues[2].xyz = kBasis3D;
+		mRowValues[3].xyz = translation3D;
+	}
+
+	Matrix44::Matrix44(Vector4 const& iBasis4D, Vector4 const& jBasis4D, Vector4 const& kBasis4D, Vector4 const& translation4D)
+		: Matrix44()
+	{
+		mRowValues[0] = iBasis4D;
+		mRowValues[1] = jBasis4D;
+		mRowValues[2] = kBasis4D;
+		mRowValues[3] = translation4D;
 	}
 }
--- MathLibrary/src/Matrix44.hpp
+++ MathLibrary/src/Matrix44.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
+#include "Vector4.hpp"
+
 namespace Math
 {
+	struct Vector2;
+	struct Vector3;
 	struct Matrix44
 	{
 		enum {
@@ -11,8 +15,22 @@ namespace Math
 			Tx, Ty, Tz, Tw
 		};
 
-		float mValues[16] = { 0 };
+		union 
+		{
+			float	mValues[16] = { 0 };
+			Vector4 mRowValues[4];
+		};
+
+		Vector4& operator[](size_t i) const
+		{
+			return (Vector4&)mRowValues[i];
+		}
 
 		Matrix44();
+		~Matrix44() {}
+
+		explicit Matrix44(Vector2 const& iBasis2D, Vector2 const& jBasis2D, Vector2 const& translation2D);
+		explicit Matrix44(Vector3 const& iBasis3D, Vector3 const& jBasis3D, Vector3 const& kBasis3D, Vector3 const& translation3D);
+		explicit Matrix44(Vector4 const& iBasis4D, Vector4 const& jBasis4D, Vector4 const& kBasis4D, Vector4 const& translation4D);
 	};
 }
